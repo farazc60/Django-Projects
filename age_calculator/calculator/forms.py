0aac9ad12663c5a53f5d19d397197f06a4210a9f
codewithfaraz <92373013+farazc60@users.noreply.github.com>
2024-08-05 20:02:19 +0530

    Delete age_calculator directory

--- age_calculator/calculator/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class AgeCalculatorForm(forms.Form):
-    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
--- age_calculator/calculator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import age_calculator_view
-
-urlpatterns = [
-    path('', age_calculator_view, name='age_calculator'),
-]
